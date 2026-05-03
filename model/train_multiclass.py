8dba3f59c597b88a2dbd2d6b0a548ceabe535063
Songlin Hou <songlin.hou@gatech.edu>
2026-05-03 04:05:24 -0400

    update scripts

--- model/train_multiclass.py
+++ model/train_multiclass.py
@@ -271,7 +271,7 @@ def main(args):
     
     # Learning rate scheduler
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, mode='max', factor=0.5, patience=5, verbose=True
+        optimizer, mode='max', factor=0.5, patience=5
     )
     
     # Training loop
