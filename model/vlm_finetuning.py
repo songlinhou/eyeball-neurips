b002ebec60a1ae41296aea8b04489ceb309e1537
Songlin Hou <songlin.hou@gatech.edu>
2026-05-03 06:17:46 -0400

    update scripts

--- model/vlm_finetuning.py
+++ model/vlm_finetuning.py
@@ -349,7 +349,7 @@ def train_vlm(
         logging_steps=logging_steps,
         save_steps=save_steps,
         eval_steps=eval_steps,
-        evaluation_strategy="steps",
+        eval_strategy="steps",  # Changed from evaluation_strategy
         save_strategy="steps",
         load_best_model_at_end=True,
         metric_for_best_model="eval_loss",
